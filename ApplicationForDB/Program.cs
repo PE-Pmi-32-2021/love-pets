8cfef148275eca05e26f02eab37b84305b841f47
Iryna.Omeliukh <iryna.omeliukh@gmail.com>
2021-10-16 21:06:58 +0300

    Add functions to fill database

--- ApplicationForDB/Program.cs
+++ ApplicationForDB/Program.cs
@@ -102,6 +102,125 @@ namespace ApplicationForDB
                 }
             }
         }
+
+        static void GenerateRandDataProfile(NpgsqlConnection con)
+        {
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                for (int i = 0; i < 30; i++)
+                {
+                    cmd.CommandText = "INSERT INTO Profile(ProfileFullname, ProfileName, Breed, Coloring, Gender, BirthDate, Age, Photolink)" +
+                        $" VALUES('ProfileFullname{i}', 'ProfileName{i}', 'Breed{i}', 'Coloring{i}',true,' 10/{i + 1}/{2000 + i}', {i}, 'Photolink{i}')";
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+
+        static void GenerateRandReminder(NpgsqlConnection con)
+        {
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                for (int i = 0; i < 30; i++)
+                {
+                    cmd.CommandText = "INSERT INTO Reminder (RemindingDate, Reminding, ReminderType, ProfileID)" +
+                        $" VALUES ('2021-11-08 22:05:06', 'Reminding{i}', 'feeding', {i + 1})";
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+
+        static void GenerateRandCategories(NpgsqlConnection con)
+        {
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                for (int i = 0; i < 30; i++)
+                {
+                    cmd.CommandText = "INSERT INTO Categories (Category, Breed, PhotoLink)" +
+                        $" VALUES ('Category{i}', 'Breed{i}', 'PhotoLink{i}')";
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+
+
+        static void GenerateRandInformation(NpgsqlConnection con)
+        {
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                for (int i = 0; i < 30; i++)
+                {
+                    cmd.CommandText = "INSERT INTO Information (InGeneral, Nutrition, Care, Health, PhotoLink1, PhotoLink2, PhotoLink3, CategoriesID)" +
+                        $" VALUES ('InGeneral{i}', 'Nutrition{i}', 'Care{i}',  'Health{i}', 'PhotoLink1{i}', 'PhotoLink2{i}', 'PhotoLink3{i}', {i + 1})";
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+        static void GenerateRandQuestions(NpgsqlConnection con)
+        {
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                for (int i = 0; i < 30; i++)
+                {
+                    cmd.CommandText = "INSERT INTO Questions (Question, QuestionType, PhotoLink)" +
+                        $" VALUES ('Question{i}', true, 'PhotoLink{i}')";
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+
+        static void GenerateRandAnswers(NpgsqlConnection con)
+        {
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                for (int i = 0; i < 30; i++)
+                {
+                    cmd.CommandText = "INSERT INTO Answers (Answer, Dogs, Cats, Rodents, Reptiles, Birds, QuestionsID)" +
+                        $" VALUES ('Answer{i}', {i}, {i},  {i}, {i}, {i}, {i + 1})";
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+
+        static void GenerateRandResults(NpgsqlConnection con)
+        {
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = con;
+                for (int i = 0; i < 30; i++)
+                {
+                    cmd.CommandText = "INSERT INTO Results (Paragraph1, Paragraph2, PhotoLink1, PhotoLink2, PhotoLink3)" +
+                        $" VALUES ('Paragraph1{i}', 'Paragraph2{i}', 'PhotoLink1{i}',  'PhotoLink2{i}', 'PhotoLink3{i}')";
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
         static void Main(string[] args)
         {
             var cs = "Host=localhost;Username=postgres;Password=1234;Database=love_pets";
