df717c8d881f9f977100bea1a105a9dfb5a6c772
Waldemar <volodymyrmorozov322@gmail.com>
2021-10-28 22:03:48 +0300

    added two functions to access photolinks in profile

--- LovePets_3tiers/LovePets_DAL/Class1.cs
+++ LovePets_3tiers/LovePets_DAL/Class1.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace LovePets_DAL
 {
     public class Class1
     {
+        static NpgsqlConnection Connect()
+        {
+            var cs = "Host=localhost;Username=postgres;Password=1234;Database=love_pets";
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            return con;
+        }
     }
 }
---
+++ LovePets_3tiers/LovePets_DAL/ProfileDO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LovePets_EF;
+
+namespace LovePets_DAL
+{
+    public class ProfileDO
+    {
+        public ProfileDO()
+        {
+
+        }
+
+        public void SaveAnimalPhoto(string photo_url, int id)
+        {
+            Database1Entities1 ne = new Database1Entities1();
+            var profile = (from u in ne.Profiles
+                          where u.ID == id
+                          select u.Photolink).ToList();
+
+            profile[0] = photo_url;
+
+            ne.SaveChanges();
+          
+        }
+
+
+        public string GetAnimalPhoto(int id)
+        {
+            Database1Entities1 ne = new Database1Entities1();
+            var profile = (from u in ne.Profiles
+                           where u.ID == id
+                           select u.Photolink).ToString();
+
+            return profile;
+        }
+
+
+
+
+    }
+}
