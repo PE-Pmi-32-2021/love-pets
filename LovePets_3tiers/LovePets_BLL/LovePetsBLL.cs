ad2d47e0572983fb1b0c34b3c37d09aa9bf66242
Iryna Sloboda <s.iryna.ya@gmail.com>
2021-10-28 22:10:35 +0300

    add functions for update and get photoLink

---
+++ LovePets_3tiers/LovePets_BLL/LovePetsBLL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LovePets_DAL;
+using LovePets_EF;
+
+namespace LovePets_BLL
+{
+    public class LovePetsBLL
+    {
+        public void UpdatePhoto(int id, string photo_link)
+        {
+           
+
+            ProfileDO profile = new ProfileDO();
+            profile.SaveAnimalPhoto(photo_link, id);
+
+                
+
+
+        }
+
+        public string GetFoteo(int id)
+        {
+            ProfileDO profile = new ProfileDO();
+            return profile.GetAnimalPhoto(id);
+        }
+
+    }
+}
